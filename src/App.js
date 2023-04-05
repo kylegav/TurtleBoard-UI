98c893165adad6cf251a4faf0202c280e484f98f
kylegav <60828879+kylegav@users.noreply.github.com>
2023-04-05 15:42:38 -0400

    Added Sider, Content, and Footer layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import OrderDetails from "./modules/OrderDetails"
 import Orders from "./modules/Orders"
 import {Routes, Route} from "react-router-dom";
+import {Layout, Image} from "antd";
+
+const {Sider, Content, Footer} = Layout;
 
 function App() {
   return (
-    <Routes>
-      <Route path="" element={<Orders/>}/>
-      <Route path="order/:id" element={<OrderDetails/>}/>
-    </Routes>
+    <Layout>
+      <Sider style={{height: '100vh', backgroundColor: "black" }}>
+        <Image src={"https://logos-world.net/wp-content/uploads/2020/11/Uber-Eats-Symbol.jpg"} preview={false}></Image>
+      </Sider>
+      <Layout>
+        <Content>
+          <Routes>
+            <Route path="" element={<Orders/>}/>
+            <Route path="order/:id" element={<OrderDetails/>}/>
+          </Routes>
+        </Content>
+        <Footer style={{textAlign: "center", backgroundColor: 'lightgray' }}>
+          Dashboard UI v0.1.0
+        </Footer>
+      </Layout>
+    </Layout>
+
   );
 }
 
